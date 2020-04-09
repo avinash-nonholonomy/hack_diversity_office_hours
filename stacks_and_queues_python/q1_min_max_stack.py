7bf90e710198999993e59c8bb634e5079ce7e9e2
avinash uttamchandani <uttamchandani.avinash@gmail.com>
2020-04-09 15:56:45 -0400

    added stubs for office hours on stacks and queues.

---
+++ stacks_and_queues_python/q1_min_max_stack.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""
+Min Max Stack Construction
+  Design a Min Max stack that supports
+  Pushing and popping values on and off the stack.
+  Peeking at the value at the top of the stack.
+  Getting both the minimum and the maximum values in the stack at any given
+point in time.
+  All operations should run in a constant time.
+
+var minMaxStack = Stack<Int>()
+minMaxStack.push(5)
+minMaxStack.getMax()
+-> returns 5
+minMaxStack.getMin()
+-> returns 5
+minMaxStack.push(2)
+minMaxStack.getMax()
+-> returns 5
+minMaxStack.getMin()
+-> returns 2
+minMaxStack.push(20)
+minMaxStack.getMax()
+-> returns 20
+minMaxStack.getMin()
+-> returns 2
+minMaxStack.peek()
+-> returns 20
+"""
+
+class Stack:
+    def __init__()
+        pass
+    def push():
+        pass
+    def pop():
+        pass
+    def peek():
+        pass
+
+class MinMaxStack(Stack):
+    def __init__(self):
+        Stack.__init__(self)
+    def getMax():
+        pass
+    def getMin():
+        pass
+        
+        
+if __name__ == "__main__":
+    min_max_stack = MinMaxStack()
+    min_max_stack.push(5)
+    print(f"min: min_max_Stack.getMax(), 
---
+++ stacks_and_queues_python/q2_queues.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""
+Implement a Queue
+Implement a class Queue that supports
+  Enqueue values on the queue.
+  Dequeue values off the queue.
+  Peek that last enqueued value of the queue.
+  All operations should run in a constant time.
+var queue = Queue<Int>()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+queue.peek()
+-> returns 1
+queue.dequeue() -> returns 1
+queue.peek()
+-> returns 2
+queue.dequeue() -> returns 2
+queue.dequeue() -> returns 3
+queue.dequeue() -> returns 4
+"""
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        pass
+    def enqueue(self, value):
+        pass
+    def dequeue():
+        pass
+    def peek():
+        pass
+    def isEmpty():
+        pass
